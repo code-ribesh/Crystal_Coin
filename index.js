ffd441ba9491c56cae5bdbef20a3622ddf1228c7
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2021-03-10 16:01:20 +0545

    V-0.0.1

---
+++ index.js
@@ -0,0 +1,107 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = __importStar(require("crypto"));
+class Transaction {
+    constructor(amount, payer, // public key
+    payee // public key
+    ) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+    }
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+class Block {
+    constructor(prevHash, transaction, ts = Date.now()) {
+        this.prevHash = prevHash;
+        this.transaction = transaction;
+        this.ts = ts;
+        this.nonce = Math.round(Math.random() * 99999999999);
+    }
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
+}
+class Chain {
+    constructor() {
+        this.chain = [new Block('', new Transaction(100, 'genesis', 'sathosi'))];
+    }
+    get lastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    mine(nonce) {
+        let solution = 1;
+        console.log('🔨 mining....');
+        while (true) {
+            const hash = crypto.createHash('MD5');
+            hash.update((nonce + solution).toString()).end();
+            const attempt = hash.digest('hex');
+            if (attempt.substr(0, 4) === '0000') {
+                console.log(`Saved: ${solution}`);
+                return solution;
+            }
+            solution += 1;
+        }
+    }
+    addBlock(transaction, senderPublicKey, signature) {
+        const verifier = crypto.createVerify('SHA256');
+        verifier.update(transaction.toString());
+        const isValid = verifier.verify(senderPublicKey, signature);
+        if (isValid) {
+            const newBlock = new Block(this.lastBlock.hash, transaction);
+            this.mine(newBlock.nonce);
+            this.chain.push(newBlock);
+        }
+    }
+}
+Chain.instance = new Chain();
+class Wallet {
+    constructor() {
+        const keyPair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+        });
+        this.privateKey = keyPair.privateKey;
+        this.publicKey = keyPair.publicKey;
+    }
+    sendMoney(amount, payeePublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction, this.publicKey, signature);
+    }
+}
+// usage
+const satoshi = new Wallet();
+const ribesh = new Wallet();
+const monika = new Wallet();
+satoshi.sendMoney(50, ribesh.publicKey);
+ribesh.sendMoney(23, monika.publicKey);
+monika.sendMoney(8, ribesh.publicKey);
+console.log(Chain.instance);
